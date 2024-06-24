f495367428665cd9bb3ca8771d96df099974dedc
MancillaO <edgarmncllav04@gmail.com>
2024-06-23 21:26:01 -0600

    carga inicial

---
+++ app/src/main/java/com/example/prolender/CrearCuentaActivity.java
@@ -0,0 +1,31 @@
+package com.example.prolender;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CrearCuentaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_cuenta);
+    }
+
+    public void onClick(View view) {
+        int id = view.getId();
+        Intent miIntent = null;
+
+        if (id == R.id.btnIniciarSesion) {
+            miIntent = new Intent(CrearCuentaActivity.this, MainActivity.class);
+        }
+
+        if (miIntent != null) {
+            startActivity(miIntent);
+        } else {
+            Toast.makeText(this, "Acción no reconocida", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/prolender/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.prolender;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        int id = view.getId();
+        Intent miIntent = null;
+
+        if (id == R.id.btnCrear) {
+            miIntent = new Intent(MainActivity.this, CrearCuentaActivity.class);
+        }
+
+        if (miIntent != null) {
+            startActivity(miIntent);
+        } else {
+            Toast.makeText(this, "Acción no reconocida", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
